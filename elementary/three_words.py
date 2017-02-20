5288af23702e23fc1df8da38fa7233cc6b45a677
Mirsait <mavcharenko@gmail.com>
2017-02-20 23:13:10 +0300

    Initial three_words.py

---
+++ elementary/three_words.py
@@ -0,0 +1,13 @@
+
+def is_get_three_words(text):
+
+
+    return true
+
+
+if __name__ == '__main__':
+    is_get_three_words("Hello World hello") == True
+    is_get_three_words("He is 123 man") == False
+    is_get_three_words("1 2 3 4") == False
+    is_get_three_words("bla bla bla bla") == True
+    is_get_three_words("Hi") == False
