f5d5892ccac48dca8caf7acb6b518919df311a92
Mirsait <mavcharenko@gmail.com>
2017-02-21 23:24:14 +0300

    "Three words is completed"

--- elementary/three_words.py
+++ elementary/three_words.py
@@ -1,9 +1,8 @@
+import re
 
 def is_get_three_words(text):
-
-
-    return true
-
+    words = re.findall('\D+\s\D+\s\D+',text, re.IGNORECASE)
+    return len(words)>0
 
 if __name__ == '__main__':
     is_get_three_words("Hello World hello") == True
@@ -11,3 +10,4 @@ if __name__ == '__main__':
     is_get_three_words("1 2 3 4") == False
     is_get_three_words("bla bla bla bla") == True
     is_get_three_words("Hi") == False
+    is_get_three_words("one two 3 four five six 7 eight 9 ten eleven 12") == True
