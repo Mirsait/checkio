ac8db82dbdb8e5ece1e0c199cdcd3c62010f30e9
Mirsait <mavcharenko@gmail.com>
2017-02-22 22:25:50 +0300

    "The most numbers" is completed

--- elementary/most_numbers.py
+++ elementary/most_numbers.py
@@ -3,16 +3,15 @@ returns the difference between the maximum and minimum numbers
 '''
 
 def get_most_numbers(*args):
-    print(args)
-    return  (max(args)-min(args)) if len(args)>0 else 0
+    return max(args)-min(args) if args else 0
 
 
-def test():
-    print(get_most_numbers(1, 2, 3) == 2)
-    print(get_most_numbers(5, -5) == 10)
-    print(get_most_numbers(10.2, -2.2, 0, 1.1, 0.5) == 12.4)
-    print(get_most_numbers() == 0)
+def test_method():
+    print(get_most_numbers(1, 2, 3) == 2)#true
+    print(get_most_numbers(5, -5) == 10)#true
+    print(get_most_numbers(10.2, -2.2, 0, 1.1, 0.5) == 12.4)#true
+    print(get_most_numbers() == 0)#true
 
 
 if __name__ == '__main__':
-    test()
+    test_method()
