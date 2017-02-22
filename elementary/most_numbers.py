c73cfaeee421954dd95f7681e92f237ba1542ea2
Mirsait <mavcharenko@gmail.com>
2017-02-22 22:05:28 +0300

    Update most_number.py

--- elementary/most_numbers.py
+++ elementary/most_numbers.py
@@ -2,11 +2,17 @@
 returns the difference between the maximum and minimum numbers
 '''
 
-def get_most_numbers(arg):
-    pass
+def get_most_numbers(*args):
+    print(args)
+    return  (max(args)-min(args)) if len(args)>0 else 0
+
+
+def test():
+    print(get_most_numbers(1, 2, 3) == 2)
+    print(get_most_numbers(5, -5) == 10)
+    print(get_most_numbers(10.2, -2.2, 0, 1.1, 0.5) == 12.4)
+    print(get_most_numbers() == 0)
+
 
 if __name__ == '__main__':
-    get_most_numbers(1, 2, 3) == 2
-    get_most_numbers(5, -5) == 10
-    get_most_numbers(10.2, -2.2, 0, 1.1, 0.5) == 12.4
-    get_most_numbers() == 0
+    test()
